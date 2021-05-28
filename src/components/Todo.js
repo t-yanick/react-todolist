0ff5d611d717d9e3a77041302483cb6b7b099994
Yanick <tyankee15@gmail.com>
2021-05-28 02:31:59 -0300

    Practice react-redux with todoApp

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from "react";
+import cx from "classnames";
+
+import { connect } from "react-redux";
+import { toggleTodo } from "../redux/actions";
+
+const Todo = ({ todo, toggleTodo }) => (
+  <li className="todo-item" onClick={() => toggleTodo(todo.id)}>
+    {todo && todo.completed ? "👌" : "👋"}{" "}
+    <span
+      className={cx(
+        "todo-item__text",
+        todo && todo.completed && "todo-item__text--completed"
+      )}
+    >
+      {todo.content}
+    </span>
+  </li>
+);
+
+// export default Todo;
+export default connect(
+  null,
+  { toggleTodo }
+)(Todo);
